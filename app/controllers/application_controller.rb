fe0029f66fc0f47f444f51e18cf19c419614b837
Kevin Price <collegekevin@gmail.com>
2022-12-15 23:06:25 -0800

    commit tree 5873d6b355e91867fef3bdbb70ebfb885ef7b343

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+    #rescue_from ActiveRecord::RecordNotFound, with: not_found
+
+    private
+
+    def unprocessable_entity(invalid)
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity 
+    end
+
+    def not_found(exception)
+    render json: {errors: "#{exception.model} not found"}, status: :not_found
+    end
+
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+   # validates :category, inclusion {in: ['Fiction', 'Non-Fiction'], message: "Categoty did not match valid categpry"}}
+    validates :content, length: { minimum: 100 }
 end
